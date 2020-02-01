baea14d4be417c30ca4316acaa25bdcd36790c58
unknown <m.mukhammadyusupova111@gmail.com>
2020-02-01 14:59:06 +0500

    step 4

--- src/Components/Main/AboutMe.js
+++ src/Components/Main/AboutMe.js
@@ -1,33 +1,17 @@
-
-// import React from 'react';
-// import Container from '@material-ui/core/Container';
-
-// // import {Container} from 'react-bootstrap/Container/Container';
-// // import { Container, Row, Col } from 'reactstrap';
-// function AboutMe() {
-
-//         return (
-//             <Container maxWidth="sm">
-//             <h1 className="title">
-//             </Container>
-                
-              
-//         );
-    
-// }
-
-// export default AboutMe;
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default function AboutMe() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        />
       </Container>
     </React.Fragment>
   );
